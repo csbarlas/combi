bab0638a793be8f017359a3b1b9abc91f6a3c788
csbarlas <62313241+csbarlas@users.noreply.github.com>
2024-02-10 15:51:40 -0500

    Initial Commit

---
+++ Combi/CombiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombiApp.swift
+//  Combi
+//
+//  Created by Christopher Barlas on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CombiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Combi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Combi
+//
+//  Created by Christopher Barlas on 2/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
